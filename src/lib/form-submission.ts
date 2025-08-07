902f75409098ed6844f82d9b62a08e3daa69cbb7
jtuluve <jtuluve@gmail.com>
2025-08-07 16:38:03 +0530

    fea: Migrated form submission from formcarry to google sheets

--- src/lib/form-submission.ts
+++ src/lib/form-submission.ts
@@ -1,20 +1,33 @@
+"use server";
+import type { CollegeFormValues, IndividualFormValues } from "./forms-config";
+import { GoogleSpreadsheet } from "google-spreadsheet";
+import { JWT } from "google-auth-library";
+const serviceAccountAuth = new JWT({
+  email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+  key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
+const doc = new GoogleSpreadsheet(
+  process.env.GOOGLE_SHEET_ID ?? "",
+  serviceAccountAuth,
+);
+
 export async function submitFormData(
-  url: string,
-  data: Record<string, string>,
+  name: "individual" | "community",
+  data: IndividualFormValues | CollegeFormValues,
 ) {
-  const formData = new FormData();
-
-  Object.entries(data).forEach(([key, value]) => {
-    formData.append(key, value);
-  });
-
-  const response = await fetch(url, {
-    method: "POST",
-    body: formData,
-    headers: {
-      Accept: "application/json",
-    },
-  });
-
-  return response;
+  try {
+    if (!doc) throw new Error("Invalid form name");
+    await doc.loadInfo();
+    const sheet =
+      doc.sheetsByIndex[
+        name === "individual" ? 0 : name === "community" ? 1 : -1
+      ];
+    if (!sheet) throw new Error("Sheet not found or invalid name");
+    await sheet.addRow(data);
+    return true;
+  } catch (error) {
+    console.error("Error submitting to Google Sheet:", error);
+    return false;
+  }
 }
