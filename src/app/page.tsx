b73d51a96084ca01e61fca4e9b11340712347e44
Gauresh G Pai <107191770+gaureshpai@users.noreply.github.com>
2025-09-12 15:34:00 +0530

    fix: Removed Testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <TeamModelSection />
         {/* <FeaturedProjects /> */}
         <UpcomingEvents />
-        <Testimonials />
+        {/* <Testimonials /> */}
       </div>
     </BackgroundPattern>
   );
