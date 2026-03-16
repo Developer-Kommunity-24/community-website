9877aa6b7912b5bb8f73a6d668866d31149a4636
risha-bit <diasminora2613@gmail.com>
2026-03-16 20:31:55 +0530

    chore: remove unused code and dependencies

--- src/constants/events.ts
+++
@@ -1,5 +0,0 @@
-/* 
-For those handling posters and recap images:
-Save each poster in the public/calendar/posters folder, using the poster's {id} as the filename.
-Save recap images in the public/calendar/{id} folder with any names you prefer.
-*/
